d61cf2bd20b87442bae6f64dea5ab7b759847aab
Justin Stafford <duckyabrams@gmail.com>
2023-10-25 13:19:41 -0700

    TaxPayer fileReturn Lab09.1

--- Lab09.1/src/com/hr/personnel/Executive.java
+++ Lab09.1/src/com/hr/personnel/Executive.java
@@ -10,4 +10,12 @@ public class Executive extends SalariedEmployee {
     public Executive(String name, LocalDate hireDate, double salary) {
         super(name, hireDate, salary);
     }
+    @Override
+    public void fileReturn() {
+        System.out.println("Return electronically.");
+    }
+    @Override
+    public void work() {
+        System.out.println(getName() + "is an executive and is playing golf");
+    }
 }
