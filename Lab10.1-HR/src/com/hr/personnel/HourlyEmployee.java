ddeeb4dc5681738cf864f351f6a5bcf03552ae89
Justin Stafford <duckyabrams@gmail.com>
2023-10-26 14:13:44 -0700

    Lab10.2 Complete

--- Lab10.1-HR/src/com/hr/personnel/HourlyEmployee.java
+++ Lab10.1-HR/src/com/hr/personnel/HourlyEmployee.java
@@ -8,6 +8,8 @@
 
 package com.hr.personnel;
 
+import gov.irs.IllegalWageException;
+
 import java.time.LocalDate;
 
 public class HourlyEmployee extends Employee {
@@ -23,7 +25,8 @@ public class HourlyEmployee extends Employee {
         super(name, hireDate);  // delegate to superclass ctor for name, hireDate
     }
 
-    public HourlyEmployee(String name, LocalDate hireDate, double rate, double hours) throws IllegalArgumentException {
+    public HourlyEmployee(String name, LocalDate hireDate, double rate, double hours)
+    throws IllegalWageException {
         this(name, hireDate);   // delegate to neighboring ctor for name, hireDate
         setRate(rate);          // handle rate here, by delegating to setter
         setHours(hours);        // handle hours here, by delegating to setter
@@ -47,9 +50,9 @@ public class HourlyEmployee extends Employee {
         return rate;
     }
 
-    public void setRate(double rate) throws IllegalArgumentException {
+    public void setRate(double rate) throws IllegalWageException {
         if (rate < FEDERAL_MINIMUM_WAGE) {
-            throw new IllegalArgumentException("Minimum wage is " + FEDERAL_MINIMUM_WAGE);
+            throw new IllegalWageException("Illegal wage is " + FEDERAL_MINIMUM_WAGE);
         } else {
 
             this.rate = rate;
--- Lab10.1-HR/src/com/hr/personnel/client/HRClient.java
+++ Lab10.1-HR/src/com/hr/personnel/client/HRClient.java
@@ -12,6 +12,8 @@ import com.hr.personnel.Department;
 import com.hr.personnel.Employee;
 import com.hr.personnel.HourlyEmployee;
 import com.hr.personnel.SalariedEmployee;
+import gov.irs.IllegalWageException;
+
 import java.time.LocalDate;
 
 /**
@@ -19,17 +21,32 @@ import java.time.LocalDate;
  */
 class HRClient {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IllegalWageException {
         // create Department object
         Department dept = new Department("Sales", "Seattle");
         System.out.println(dept);
 
         // add Employees to it
         dept.addEmployee(new SalariedEmployee("Mew",  LocalDate.of(1990, 8, 24), 2250.0));
-        dept.addEmployee(new HourlyEmployee("Aaron",    LocalDate.of(2000, 2, 2), 14.99, 30.0));
-        dept.addEmployee(new SalariedEmployee("Justin", LocalDate.of(1999, 2, 6), 1250.0));
-        dept.addEmployee(new HourlyEmployee("Oliver",    LocalDate.of(2002, 6, 2), 40.0, 40.0));
+        dept.addEmployee(new SalariedEmployee("Justin", LocalDate.of(1999, 2, 6), 1_250.0));
         dept.addEmployee(new SalariedEmployee("Jeffrey", LocalDate.of(2004, 1, 2), 5_000.0));
+
+        // add Employees to it
+        try {
+            dept.addEmployee(new HourlyEmployee("Aaron",    LocalDate.of(2000, 2, 2), 5, 30.0));
+            }
+
+        catch (IllegalWageException e) {
+            System.out.println("Error: " + e);
+        }
+
+        try {
+            dept.addEmployee(new HourlyEmployee("Oliver",    LocalDate.of(2002, 6, 2), 40.0, 40.0));
+        }
+        catch (IllegalWageException e) {
+            System.out.println("Error: " + e);  // e.toString() authomatically called
+        }
+
 
         // list its Employees
         System.out.println("\nList employees:");
